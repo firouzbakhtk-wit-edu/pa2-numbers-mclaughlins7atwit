28ab2761fd6d85d093a4e08b090bb4737ecbc660
Sean McLaughlin <mclaughlins7@wit.edu>
2024-10-10 22:58:57 -0400

    Solutions to PA2 part A and part B

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,79 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
+/**
+ * Solution to PA2 part a assignment
+ * When ran it gets the input of 5 numbers and outputs the sum and average of the positive, negative, and all numbers
+ * @author Sean McLaughlin
+ */
 public class PA2a {
-
-	// TODO: document this function
+	/**
+	 * Starts the program, asks for user input
+	 * @param args command-line arguments ignored
+	 */
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		int positiveSum =0;
+		int negativeSum=0;
+		int positiveCount =0;
+		int negativeCount =0;
+		int totalSum =0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter five whole numbers: ");
+		//For loop
+		for (int a = 0; a < 5; a++) {
+			int userInput = input.nextInt();
+			totalSum += userInput;
+			//if statements
+			if (userInput > 0) {
+				positiveSum += userInput;
+				positiveCount++;
+			} else {
+				negativeSum += userInput;
+				negativeCount++;
+			}
+		}
+		double positiveAverage =0.0;
+		if (positiveCount > 0) {
+			positiveAverage = (double) positiveSum / positiveCount;
+		}
+		double negativeAverage =0.0;
+		if (negativeCount > 0) {
+			negativeAverage = (double) negativeSum / negativeCount;
+		}
+		double totalAverage = (double) totalSum / 5;
+			
+		if (positiveCount == 1) {
+			System.out.printf("The sum of the %d positive number: %d\n", positiveCount, positiveSum);
+		} else {
+			System.out.printf("The sum of the %d positive numbers: %d\n", positiveCount, positiveSum);
+	
+		}
+		if (negativeCount == 1) {
+			System.out.printf("The sum of the %d non-positive number: %d\n", negativeCount, negativeSum);
+		} else {
+			System.out.printf("The sum of the %d non-positive numbers: %d\n", negativeCount, negativeSum);
+		}
+		
+		System.out.printf("The sum of the 5 numbers: %d\n", totalSum);
+		
+		if (positiveCount == 1) {
+			System.out.printf("The average of the %d positive number: %.2f\n", positiveCount, positiveAverage);
+		} else {
+			System.out.printf("The average of the %d positive numbers: %.2f\n", positiveCount, positiveAverage);
+		}
+		if (negativeCount == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f\n", negativeCount, negativeAverage);
+		} else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f\n", negativeCount, negativeAverage);
+		}
+		System.out.printf("The average of the 5 numbers: %.2f\n", totalAverage);
+		input.close();
 	}
-
+		
+		
 }
+
+
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,53 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
-public class PA2b {
+import java.util.Scanner;
 
-	// TODO: document this function
+/**
+ * Solution to PA2 part B assignment
+ * 
+ * @author Sean McLaughlin
+ */
+
+public class PA2b {
+	/**
+	 * Starts the program, asks for user input
+	 * @param args command-line arguments ignored
+	 */
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		double a;
+		double b;
+		double c;
+		int exponent = 2;
+		
+		double[] array = new double[3];
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter a b c: ");
+		for(int i =0; i < array.length; i++) {
+			double number = input.nextDouble();
+			array[i] = number;
+		}
+		
+		a = array[0];
+		b = array[1];
+		c = array[2];
+		
+		
+		double discriminant = Math.pow(b, exponent) - (4*a*c);
+		if (discriminant < 0) {
+			System.out.printf("Roots: imaginary\n");
+		} else if (discriminant ==0) {
+			double root = -b/ (2*a);
+			System.out.printf("Root: %.2f\n", root);
+		} else {
+			double quadraticFormulaPositive = (-b + Math.sqrt(discriminant)) / (2*a);
+			double quadraticFormulaNegative = (-b - Math.sqrt(discriminant)) / (2*a);
+			
+			System.out.printf("Roots: %.2f, %.2f\n", quadraticFormulaPositive, quadraticFormulaNegative);
+		}
+		
+		input.close();
 	}
 
 }
